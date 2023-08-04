6d8c4ab39484300438abaa0a93ab9993e66b8c70
portal222 <dvaportala@gmail.com>
2023-08-04 17:09:33 +0200

    jezik

--- src/components/SearchResultsGeog.js
+++ src/components/SearchResultsGeog.js
@@ -75,44 +75,63 @@ const SearchResutsGeog = () => {
                             </td>
                         </tr>
                         <tr>
-                            <td >Name</td>
+                            <td >Name:</td>
                             <td className="nameComm">{dataObj.name.common}</td>
 
                         </tr>
                         <tr>
-                            <td >Official name</td>
+                            <td >Official name:</td>
                             <td className="nameOff">{dataObj.name.official}</td>
                         </tr>
                         <tr>
-                            <td >Serbian</td>
+                            <td >Serbian:</td>
                             <td className="nameOff"> {dataObj.translations.srp.official}</td>
                         </tr>
                         <tr>
-                            <td >Capital</td>
+                            <td >Capital:</td>
                             <td className="capital">{dataObj.capital}</td>
                         </tr>
                         <tr>
-                            <td >Region</td>
+                            <td >Region:</td>
                             <td>{dataObj.region}</td>
                         </tr>
                         <tr>
-                            <td >Subregion</td>
+                            <td >Subregion:</td>
                             <td>{dataObj.subregion}</td>
                         </tr>
                      <tr>
-                        <td>Demonyms</td>
+                        <td>Demonyms:</td>
                         <td>{dataObj.demonyms.eng.m}</td>
                      </tr>
+                     <tr>
+                        <td>Languages:</td>
+                        <td>{ dataObj.languages.jpn  ||dataObj.languages.que || dataObj.languages.grn || dataObj.languages.pau
+                        || dataObj.languages.nep   || dataObj.languages.urd || dataObj.languages.heb || dataObj.languages.ber || dataObj.languages.hin
+                        || dataObj.languages.ara || dataObj.languages.pus || dataObj.languages.tuk || dataObj.languages.est
+                        || dataObj.languages.vie  || dataObj.languages.de || dataObj.languages.kaz || dataObj.languages.lav
+                        || dataObj.languages.rus || dataObj.languages.ita || dataObj.languages.sqi || dataObj.languages.srp || dataObj.languages.zho
+                        || dataObj.languages.nld || dataObj.languages.hrv || dataObj.languages.mkd || dataObj.languages.bos || dataObj.languages.pol 
+                        || dataObj.languages.por  || dataObj.languages.slv || dataObj.languages.ron ||dataObj.languages.lit ||dataObj.languages.cat
+                        || dataObj.languages.bul || dataObj.languages.ell  || dataObj.languages.kal || dataObj.languages.ces || dataObj.languages.slk
+                        || dataObj.languages.mon || dataObj.languages.cnr || dataObj.languages.hun || dataObj.languages.kor 
+                         || dataObj.languages.nor || dataObj.languages.fin || dataObj.languages.swe || dataObj.languages.ind
+                         || dataObj.languages.spa || dataObj.languages.deu || dataObj.languages.fra 
+                        || dataObj.languages.eng}</td>
+                     </tr>
                         <tr>
-                            <td >Population</td>
+                            <td >Population:</td>
                             <td className="population">{dataObj.population}</td>
                         </tr>
+                        <tr>
+                            <td>Area sqkm:</td>
+                            <td className="population">{dataObj.area}</td>
+                        </tr>
                            <tr>
-                            <td>Lon Lat</td>
+                            <td>Lon Lat:</td>
                             <td className="long">{dataObj.latlng[0] + " " + dataObj.latlng[1]}</td>
                         </tr>
                         <tr className="region">
-                            <td className="maps">Googlemaps</td>
+                            <td className="maps">Googlemaps:</td>
                             <td className="maps">
                                 <a href={dataObj.maps.googleMaps} target='_blank' >
                                     maps
