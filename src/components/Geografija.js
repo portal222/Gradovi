3234afd4a0d676abaa095121d900a6e2f0a3110b
portal222 <dvaportala@gmail.com>
2023-08-11 12:50:18 +0200

    ikona

--- src/components/Geografija.js
+++ src/components/Geografija.js
@@ -33,6 +33,7 @@ const Geografija = () => {
 
     const getCountry = async () => {
         const url = "https://restcountries.com/v3.1/all";
+        
         const response = await fetch(url);
         const data = await response.json();
         console.log("ZEMlje", data);
